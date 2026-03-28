c46bdcebb1020e3afac57ed04bbd1acdbaf4657a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 10:58:33 +0530

    Added Footer component with React Bootstrap layout structure

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,13 @@ import { Col, Container, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 
-
+const Footer = () => {
+  return (
+    <Container>
+      <hr />
+      <Row className="mt-5 mb-5 justify-content-center">
+        <Col md={{ span: 3, offset: 1 }}>
+          <h4>User</h4>
 
 
 
