0ccdca657c9396f0c287405240300a2e7a3ad2f8
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 11:02:56 +0530

    Added TopNavigation component with Navbar and brand link to home

--- src/components/layout/TopNavigation.jsx
+++ src/components/layout/TopNavigation.jsx
@@ -1,2 +1,36 @@
 import { Nav, Container, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+
+const TopNavigation = () => {
+  return (
+    <>
+      <Navbar sticky="top" bg="light" variant="light" className="mb-4">
+        <Container>
+          <Navbar.Brand as={NavLink} to="/">
+            Simple Client
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
