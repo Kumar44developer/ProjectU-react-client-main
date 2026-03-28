e68b3a336b4e853ce4b613095800db3906d5ed6a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 11:09:11 +0530

    Added createUser function to call API endpoint for creating a user

--- src/components/services/user.service.js
+++ src/components/services/user.service.js
@@ -1,3 +1,66 @@
 import axios from "axios";
 
 const baseApiUrl = "http://localhost:4000/v1";
+
+
+export const createUser = async (payload) => {
+  const createUserEndpoint = `${baseApiUrl}/user`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
