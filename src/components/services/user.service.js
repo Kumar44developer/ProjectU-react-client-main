008a0c995b1c840e930bb0122030ffdaf6afe7c3
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 11:09:36 +0530

    Implemented createUser function using Axios POST and return API response

--- src/components/services/user.service.js
+++ src/components/services/user.service.js
@@ -7,6 +7,11 @@ export const createUser = async (payload) => {
   const createUserEndpoint = `${baseApiUrl}/user`;
 
 
+  const { data: apiResponse } = await axios.post(createUserEndpoint, payload);
+
+  return apiResponse;
+};
+
 
 
 
