c79852c7d808ba720dc5e1a1f5c2caf2c4fce639
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 11:00:47 +0530

    Imported Footer and TopNavigation components for layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,3 +2,6 @@ import { ToastContainer } from 'react-toastify';
 import { Container } from 'react-bootstrap';
 import { createGlobalStyle } from 'styled-components';
 
+
+import Footer from './Footer';
+import TopNavigation from './TopNavigation';
