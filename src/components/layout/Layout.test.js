4d9ae06ab7639fda12d51978621d1b6644b36134
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 11:02:14 +0530

    Added basic test for Layout component rendering with children

--- src/components/layout/Layout.test.js
+++ src/components/layout/Layout.test.js
@@ -1,3 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Layout from './Layout';
 
+
+test('renders Layout', () => {
+  const expectedContents = 'Hello';
+  render(<Layout>{expectedContents}</Layout>);
+  expect(screen.queryByText(layoutContents)).toBeNull();
+});
