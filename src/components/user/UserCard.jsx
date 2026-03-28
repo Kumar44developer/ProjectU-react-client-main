5fa7f621fc5866cd1d6107b1b96599555849bf76
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-28 11:04:26 +0530

    Added UserCard component displaying user's name in Card layout

--- src/components/user/UserCard.jsx
+++ src/components/user/UserCard.jsx
@@ -1,3 +1,37 @@
 import { Button, Card } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
+const UserCard = ({ user }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <h4>{user.name}</h4>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
