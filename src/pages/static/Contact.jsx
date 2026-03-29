d8e05d1b7f931ca7d4fe5d98efb9030951d70a9e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-29 21:15:22 +0530

    Updated Contact component with email information

--- src/pages/static/Contact.jsx
+++ src/pages/static/Contact.jsx
@@ -7,7 +7,9 @@ const Contact = () => {
     <Layout>
       <h3 className="text-center">Contact Us</h3>
       <Row className="justify-content-center">
-
+        <Col md={10}>
+          <p class="text-center">
+            Contact me at <span className="fst-italic">MY EMAIL ADDRESS</span>
 
 
 
