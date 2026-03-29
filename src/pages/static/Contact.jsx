0c7830170e65c64f40a2de1f8e3e0e4c9b052464
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-29 21:15:03 +0530

    Added Contact component with layout and heading

--- src/pages/static/Contact.jsx
+++ src/pages/static/Contact.jsx
@@ -1,2 +1,56 @@
 import Layout from "../../components/layout/Layout";
 import { Col, Row } from "react-bootstrap";
+
+
+const Contact = () => {
+  return (
+    <Layout>
+      <h3 className="text-center">Contact Us</h3>
+      <Row className="justify-content-center">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
