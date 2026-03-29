72bee2caf9b673c595fbe7777835ebc4f4dbb368
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-29 21:27:59 +0530

    Imported userService in EditUser component

--- src/pages/user/EditUser.jsx
+++ src/pages/user/EditUser.jsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout/Layout";
 import { firstUpperCase } from "../../helpers/string.helper";
-
+import * as userService from "../../services/user.service";
+import { useEffect, useState } from "react";
 
 
 
