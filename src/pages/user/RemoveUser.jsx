a0a0c3e7206b32dc6fd89cfa28ec4976b3d6160f
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-30 15:28:39 +0530

    Imported React-Bootstrap components, useParams, and toast

--- src/pages/user/RemoveUser.jsx
+++ src/pages/user/RemoveUser.jsx
@@ -1,2 +1,5 @@
 import Layout from "../../components/layout/Layout";
 import * as userService from "../../services/user.service";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
