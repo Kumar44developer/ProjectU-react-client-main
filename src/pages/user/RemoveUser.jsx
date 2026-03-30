36d7966f54b9d9b7624295f65d7ccd5ae78cd17b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-30 15:29:11 +0530

    Updated RemoveUser.jsx

--- src/pages/user/RemoveUser.jsx
+++ src/pages/user/RemoveUser.jsx
@@ -3,3 +3,45 @@ import * as userService from "../../services/user.service";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+
+const RemoveUser = () => {
+  const DELAY_BEFORE_REDIRECTION_MS = 1000; // 1 second
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
