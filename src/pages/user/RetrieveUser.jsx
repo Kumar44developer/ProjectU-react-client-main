912597d1044bf73d9e3f77f730a2948de04b4f6a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-31 13:57:44 +0530

    imported { useParams } from "react-router-dom";  import Layout from "../../components/layout/Layout"; import * as userService from "../../services/user.service";

--- src/pages/user/RetrieveUser.jsx
+++ src/pages/user/RetrieveUser.jsx
@@ -1,3 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
+import Layout from "../../components/layout/Layout";
+import * as userService from "../../services/user.service";
