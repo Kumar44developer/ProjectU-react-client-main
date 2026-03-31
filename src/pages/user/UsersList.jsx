670b424c4c2dfaf84098f9c02d19c4a99d29ce05
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-03-31 18:08:28 +0530

    imported React, hooks, react-bootstrap components, userService, and List loader

--- src/pages/user/UsersList.jsx
+++ src/pages/user/UsersList.jsx
@@ -1,2 +1,6 @@
 import Layout from '../../components/layout/Layout';
 import UserCard from '../../components/user/UserCard';
+import * as userService from '../../services/user.service';
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { List } from 'react-content-loader';
