7c50a2d44bf0011d093ce60015943580558c02e2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-01 13:16:13 +0530

    Imported EditUser, RemoveUser, and RetrieveUser components for user operations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
 import AboutUs from "./pages/static/AboutUs";
 import Contact from "./pages/static/Contact";
 import CreateUser from "./pages/user/CreateUser";
+import EditUser from "./pages/user/EditUser";
+import RemoveUser from "./pages/user/RemoveUser";
+import RetrieveUser from "./pages/user/RetrieveUser";
